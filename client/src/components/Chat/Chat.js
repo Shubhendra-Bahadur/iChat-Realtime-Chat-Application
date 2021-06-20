61e0e301e1130bf0f224cb9dc32256f8a5f4220f
Shubhendra Srivastava <shubhendrabahadur6@gmail.com>
2021-06-20 16:18:17 +0530

    end point change

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -15,7 +15,7 @@ function Chat({ location }) {
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
   const [users, setUsers] = useState([]);
-  const ENDPOINT = "localhost:5000";
+  const ENDPOINT = "https://ichat-realtine-chat-app.herokuapp.com";
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
